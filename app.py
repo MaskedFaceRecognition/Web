a37487389b02f3bfce21108254ab4257912cdcaf
copes <copes.km@gmail.com>
2021-04-28 22:49:12 +0900

    image file upload, model load

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
-# from keras.models import load_model
-# import cv2
+from keras.models import load_model
+import cv2
+from scipy import misc
 
 
 app = Flask(__name__)
@@ -9,42 +10,49 @@ app = Flask(__name__)
 def hello():
     return render_template("hello.html")
 
-# Emotion - 예측해서 GAN에 활용할 사진 업로드 or 캡처
-@app.route("/multi_upload_emotion")
+# Emotion - 예측해서 GAN에 활용할 사진(#.jpg) 업로드 or 캡처 
+@app.route("/multi_upload_emotion", methods = ['POST'])
 def multi_upload_emotion():
     uploaded_files = request.files.getlist("file[]")
     IDX = 0
     for file in uploaded_files:
-        file.save("static/Emotion/{}.jpeg".format(IDX))
+        file.save("static/Emotion/{}.jpg".format(IDX))
         IDX += 1
     return redirect(url_for("hello"))
 
-# GAN - 복원시킬 사진 업로드
+# GAN - 복원시킬 사진(#.jpg) 업로드
 @app.route("/multi_upload_gan", methods = ["POST"])
 def multi_upload_gan():
     uploaded_files = request.files.getlist("file[]")
     IDX = 0
     for file in uploaded_files:
-        file.save("static/gan/{}.jpeg".format(IDX))
+        file.save("static/GAN/{}.jpg".format(IDX))
         IDX += 1
     return redirect(url_for("hello"))
 
-'''
-@app.route("/emotion_classification")
+
+@app.route("/emotion_classification", methods = ['POST'])
 def emotion_prediction():
     # 1. 업로드 이미지 reshape
     # 2. 모델 불러와서 업로드 이미지 학습
     # 3. 예측된 표정에 맞는 곳에 저장(GAN/happy, GAN/sad ..etc)
     if request.method == 'POST':
-        file = request.files['static/Emot
-ion']
-         따로 처리 필요
-        # if not file:
-            # return render_template('index_html', label = 'No Files')
-    src = cv2.imread(file)
-    img = cv2.resize(src, dsize = (48, 48))
-    prediction = model.predict(img)
-'''
+        file = request.files['static/Emotion/image1.jpg'] # 일단 파일 1개
+        if not file:
+            return render_template('index_html', label = 'No Files')
+    
+        img = misc.imread(file)
+        img = img[:, :, :3]
+        img = img.reshape(-1, 1)
+
+        prediction = model.predict(img)
+
+        label = str(np.squeeze(prediction))
+
+        if label == '10': label = '0'
+
+        return render_template('index_html', label = label)
+
 
 @app.route("/happy")
 def happy():
@@ -64,5 +72,5 @@ def restore():
 
 
 if __name__ == "__main__":
-    # model = load_model('model_bestweight_0_2.h5') # model load
+    model = load_model('models/model_best_0_2.h5') # model load
     app.run()
